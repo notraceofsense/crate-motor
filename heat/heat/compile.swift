92ac3cee537cd29c5ac71f5d54d2b5acc9b5e232
Kotone <peridotrocketscientist@gmail.com>
2025-09-01 16:11:54 -0400

    added notes from actual solaris box

--- heat/heat/compile.swift
+++ heat/heat/compile.swift
@@ -21,3 +21,25 @@ func compile_e(_ e: Ast) -> [flame.Instr] {
         return []
     }
 }
+
+
+/*
+ $ cat foo.s
+         .file   "foo.c"
+         .section        ".text"
+         .align 4
+         .global foo
+         .type   foo, #function
+         .proc   04
+ foo:
+         !#PROLOGUE# 0
+         save    %sp, -112, %sp
+         !#PROLOGUE# 1
+         mov     1, %g1
+         mov     %g1, %i0
+         ret
+         restore
+         .size   foo, .-foo
+         .ident  "GCC: (GNU) 3.4.3 (csl-sol210-3_4-20050802)"
+ 
+ */
